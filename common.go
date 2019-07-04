261bddb8f960dd30863508a91ae826b1e600fa6b
Marten Seemann <martenseemann@gmail.com>
2019-07-04 15:13:51 +0700

    export the 0-RTT read key

--- common.go
+++ common.go
@@ -119,6 +119,7 @@ type EncryptionLevel uint8
 
 const (
 	EncryptionHandshake EncryptionLevel = iota
+	Encryption0RTT
 	EncryptionApplication
 )
 
@@ -662,6 +663,7 @@ type Config struct {
 
 	// If MaxEarlyData is greater than 0, the client will be allowed to send early
 	// data when resuming a session.
+	// Requires the AlternativeRecordLayer to be set.
 	//
 	// It has no meaning on the client.
 	MaxEarlyData uint32
@@ -1178,6 +1180,7 @@ func (c *Config) BuildNameToCertificate() {
 
 const (
 	keyLogLabelTLS12           = "CLIENT_RANDOM"
+	keyLogLabelEarlyTraffic    = "CLIENT_EARLY_TRAFFIC_SECRET"
 	keyLogLabelClientHandshake = "CLIENT_HANDSHAKE_TRAFFIC_SECRET"
 	keyLogLabelServerHandshake = "SERVER_HANDSHAKE_TRAFFIC_SECRET"
 	keyLogLabelClientTraffic   = "CLIENT_TRAFFIC_SECRET_0"
--- handshake_server_tls13.go
+++ handshake_server_tls13.go
@@ -323,6 +323,17 @@ func (hs *serverHandshakeStateTLS13) checkForResumption() error {
 			return err
 		}
 
+		h := cloneHash(hs.transcript, hs.suite.hash)
+		h.Write(hs.clientHello.marshal())
+		if sessionState.maxEarlyData > 0 && c.config.MaxEarlyData > 0 {
+			clientEarlySecret := hs.suite.deriveSecret(hs.earlySecret, "c e traffic", h)
+			c.in.exportKey(Encryption0RTT, hs.suite, clientEarlySecret)
+			if err := c.config.writeKeyLog(keyLogLabelEarlyTraffic, hs.clientHello.random, clientEarlySecret); err != nil {
+				c.sendAlert(alertInternalError)
+				return err
+			}
+		}
+
 		hs.hello.selectedIdentityPresent = true
 		hs.hello.selectedIdentity = uint16(i)
 		hs.usingPSK = true
