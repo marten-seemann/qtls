8de288bd4ce94a9bac84c5b4b323fa5ce9760c09
Marten Seemann <martenseemann@gmail.com>
2019-07-05 16:07:59 +0700

    add the option to encode application data into the session ticket

--- handshake_server_tls13.go
+++ handshake_server_tls13.go
@@ -758,7 +758,7 @@ func (hs *serverHandshakeStateTLS13) sendSessionTickets() error {
 		return nil
 	}
 
-	m, err := hs.c.getSessionTicketMsg()
+	m, err := hs.c.getSessionTicketMsg(nil)
 	if err != nil {
 		return err
 	}
