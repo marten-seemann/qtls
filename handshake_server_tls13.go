dea7754f6cf42dada58d02fb9fe7aa6bf1e4c618
Marten Seemann <martenseemann@gmail.com>
2020-01-29 15:18:22 +0700

    reject 0-RTT when a different ALPN is chosen

--- handshake_server_tls13.go
+++ handshake_server_tls13.go
@@ -227,6 +227,13 @@ GroupSelection:
 		c.config.ReceivedExtensions(typeClientHello, hs.clientHello.additionalExtensions)
 	}
 
+	if len(hs.clientHello.alpnProtocols) > 0 {
+		if selectedProto, fallback := mutualProtocol(hs.clientHello.alpnProtocols, c.config.NextProtos); !fallback {
+			hs.encryptedExtensions.alpnProtocol = selectedProto
+			c.clientProtocol = selectedProto
+		}
+	}
+
 	return nil
 }
 
@@ -276,7 +283,8 @@ func (hs *serverHandshakeStateTLS13) checkForResumption() error {
 				return errors.New("tls: client sent unexpected early data")
 			}
 
-			if c.config.Accept0RTT != nil && c.config.Accept0RTT(sessionState.appData) {
+			if sessionState.alpn == c.clientProtocol &&
+				c.config.Accept0RTT != nil && c.config.Accept0RTT(sessionState.appData) {
 				hs.encryptedExtensions.earlyData = true
 				c.used0RTT = true
 			}
@@ -592,12 +600,6 @@ func (hs *serverHandshakeStateTLS13) sendServerParameters() error {
 		return err
 	}
 
-	if len(hs.clientHello.alpnProtocols) > 0 {
-		if selectedProto, fallback := mutualProtocol(hs.clientHello.alpnProtocols, c.config.NextProtos); !fallback {
-			hs.encryptedExtensions.alpnProtocol = selectedProto
-			c.clientProtocol = selectedProto
-		}
-	}
 	if hs.c.config.GetExtensions != nil {
 		hs.encryptedExtensions.additionalExtensions = hs.c.config.GetExtensions(typeEncryptedExtensions)
 	}
--- record_layer_test.go
+++ record_layer_test.go
@@ -578,3 +578,84 @@ func TestZeroRTTRejection(t *testing.T) {
 		})
 	}
 }
+
+func TestZeroRTTRejectionOnALPNMismatch(t *testing.T) {
+	raddr := &net.TCPAddr{IP: net.IPv4(127, 0, 0, 1), Port: 1234}
+	sIn := make(chan []byte, 10)
+	sOut := make(chan []byte, 10)
+
+	// do a first handshake and encode a "foobar" into the session ticket
+	errChan := make(chan error, 1)
+	go func() {
+		serverConf := testConfig.Clone()
+		serverConf.NextProtos = []string{"proto1"}
+		serverConf.AlternativeRecordLayer = &recordLayer{in: sIn, out: sOut}
+		serverConf.MaxEarlyData = 1
+		server := Server(&unusedConn{remoteAddr: raddr}, serverConf)
+		defer server.Close()
+		err := server.Handshake()
+		if err != nil {
+			errChan <- err
+			return
+		}
+		st, err := server.GetSessionTicket(nil)
+		if err != nil {
+			errChan <- err
+			return
+		}
+		sOut <- st
+		errChan <- nil
+	}()
+
+	clientConf := testConfig.Clone()
+	clientConf.NextProtos = []string{"proto1"}
+	clientConf.AlternativeRecordLayer = &recordLayer{in: sOut, out: sIn}
+	clientConf.ClientSessionCache = NewLRUClientSessionCache(10)
+	client := Client(&unusedConn{remoteAddr: raddr}, clientConf)
+	if err := client.Handshake(); err != nil {
+		t.Fatalf("first handshake failed %s", err)
+	}
+	if err := <-errChan; err != nil {
+		t.Fatalf("first handshake failed %s", err)
+	}
+	if err := client.HandlePostHandshakeMessage(); err != nil {
+		t.Fatalf("handling the session ticket failed: %s", err)
+	}
+	client.Close()
+
+	// now dial the second connection
+	errChan = make(chan error, 1)
+	connStateChan := make(chan ConnectionState, 1)
+	go func() {
+		serverConf := testConfig.Clone()
+		serverConf.NextProtos = []string{"proto2"}
+		serverConf.AlternativeRecordLayer = &recordLayer{in: sIn, out: sOut}
+		serverConf.Accept0RTT = func([]byte) bool { return true }
+		server := Server(&unusedConn{remoteAddr: raddr}, serverConf)
+		defer server.Close()
+		errChan <- server.Handshake()
+		connStateChan <- server.ConnectionState()
+	}()
+
+	clientConf.Enable0RTT = true
+	var rejected bool
+	clientConf.Rejected0RTT = func() { rejected = true }
+	clientConf.NextProtos = []string{"proto2"}
+	client = Client(&unusedConn{remoteAddr: raddr}, clientConf)
+	if err := client.Handshake(); err != nil {
+		t.Fatalf("second handshake failed %s", err)
+	}
+	defer client.Close()
+	if err := <-errChan; err != nil {
+		t.Fatalf("second handshake failed %s", err)
+	}
+	if !rejected {
+		t.Fatal("expected 0-RTT to be rejected")
+	}
+	if client.ConnectionState().Used0RTT {
+		t.Fatal("expected 0-RTT to be rejected")
+	}
+	if (<-connStateChan).Used0RTT {
+		t.Fatal("expected 0-RTT to be rejected")
+	}
+}
